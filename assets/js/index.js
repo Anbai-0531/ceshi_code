45275c1cc53ca0d61588dcc07fc0e87f182d16a3
Anbai <599620638@qq.com>
2020-12-07 11:51:23 +0800

    开发未完成index界面

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,27 @@
 $(function() {
+    layer = layui.layer
+    $('btnLogout').on('click', function) {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function() {
 
-})
+        })
+    }
 
-function getUserInfo() {
-    $.ajax({
-        method: 'GET',
-        url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
-        success: function(res) {
-            if (res.status !== 0) {
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
 
-                return layui.layer.msg('获取用户信息失败')
+            success: function(res) {
+                if (res.status !== 0) {
+
+                    return layui.layer.msg('获取用户信息失败')
+                }
+
             }
+        });
+
+        function renderAvatar() {
 
         }
-    });
-
-    function renderAvatar() {
-
     }
-}
+})
